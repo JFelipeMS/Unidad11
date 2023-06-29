4085329cb0156d2d0366ee748a03ccdecc864f27
jfelipe <jfmoncada@udistrital.edu.co>
2023-06-28 21:51:42 -0500

    crud musql 04

--- CrudMysql/app.js
+++ CrudMysql/app.js
@@ -12,11 +12,22 @@ const PORT = process.env.PORT || 8081
 const conexion = require('./database/db')
 const crud = require('./controller/crud')
 
-//rutas
+server.use(express.urlencoded({extended:false}))
+server.use(express.json())
+server.use(express.static('./database'))    //modelo
+server.use(express.static('./views'))       //vista
+server.use(express.static('./controller'))  //controlador
+
+//Inicio Rutas
 server.get('/', crud.consultar)
 server.get('/crear', (req,res) =>{
     res.render('create')
 })
+server.post('/salvar',crud.save)
+server.get('/editar/:id', crud.consultaruno)
+server.post('/actualizar',crud.actualizar)
+server.get('/borrar/:id',crud.delete)
+//fin Rutas
 
 server.listen(PORT, () => {
     console.log("servidor funcionando en http://localhost:"+PORT)
